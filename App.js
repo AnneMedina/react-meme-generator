3d204d6b68fbf7a2ce00fff7f9dcee45775957e0
Anne Medina <carmelaanne.medina@gmail.com>
2024-10-15 13:44:39 +1030

    React Course Project 3 - Header component

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from "react"
+import Header from "./Header"
 
 /**
  * Challenge: Build the Header component
  */
 export default function App() {
-    return <h1>Hello world!</h1>
+    // return <h1>Hello world!</h1>
+    return (
+        <div>
+            <Header />
+        </div>
+    )
 }
