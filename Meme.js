5425ff18cff51d153c138fe79c22f690af5e9e28
Anne Medina <carmelaanne.medina@gmail.com>
2024-10-17 00:12:14 +1030

    Log randmon url in the console when clicking new image button

--- Meme.js
+++ Meme.js
@@ -1,8 +1,26 @@
 import React from "react";
+import memesData from "./memesData.js";
+
+
+
+function getMemeImage() {
+    const memesArr = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArr.length);
+    console.log(memesArr[randomNumber].url);
+}
+
 
 export default function Meme() {
+
+    /**
+     * Challenge: Get a random image from the `memesData` array
+     * when the "new meme image" button is clicked.
+     * 
+     * Log the URL of the image to the console. (Don't worry
+     * about displaying the image yet)
+     */
     return (
-        <form className="meme">
+        <div className="meme">
             <div className="meme--inputs">
                 <label>Top text
                     <input type="text" className="meme--input" placeholder="Shut up"></input>
@@ -15,7 +33,7 @@ export default function Meme() {
                 </label>
             </div>
 
-            <button className="meme--button">Get a new meme image  🖼 <img src="" className="button-logo" /></button>
-        </form>
+            <button onClick={getMemeImage} className="meme--button">Get a new meme image  🖼 <img src="" className="button-logo" /></button>
+        </div>
     )
 }
